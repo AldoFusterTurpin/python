9adf2f089dfb34892133785332649d857125ca73
Aldo <38758944+AldoFusterTurpin@users.noreply.github.com>
2021-07-27 22:05:14 +0200

    Create add_subtraction_product.py

---
+++ hackerRank/add_subtraction_product.py
@@ -0,0 +1,15 @@
+# ALDO FUSTER TURPIN
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    my_sum = a + b
+    print(my_sum)
+    
+    my_subtraction = a - b
+    print(my_subtraction)
+    
+    my_prodduct = a * b
+    print(my_prodduct)
+    

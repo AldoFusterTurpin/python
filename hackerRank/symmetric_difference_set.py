e1efd30ab65bc4c1d9063ad692c5593a384f7524
Aldo <38758944+AldoFusterTurpin@users.noreply.github.com>
2021-07-30 23:20:20 +0200

    Create symmetric_difference_set.py

---
+++ hackerRank/symmetric_difference_set.py
@@ -0,0 +1,18 @@
+# ALDO FUSTER TURPIN
+# don't need to convert input to int, can work with strings
+
+import sys
+
+n_suscribed_to_english_paper = input()
+suscribed_to_english_paper = input().split()
+
+n_suscribed_to_french_paper = input()
+suscribed_to_french_paper = input().split()
+
+suscribed_to_english_paper_set = set(suscribed_to_english_paper)
+suscribed_to_french_paper_set= set(suscribed_to_french_paper)
+
+result = set(suscribed_to_english_paper_set.symmetric_difference(suscribed_to_french_paper_set))
+result_length = len(result)
+
+print(result_length)
